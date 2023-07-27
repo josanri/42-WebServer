30c2ac1f964bb737c1d84125606324959cf0fed1
aaguiler <aguileragarciadev@gmail.com>
2023-07-27 11:13:21 +0200

    Change port to 8080

--- src/HttpPortListener.cpp
+++ src/HttpPortListener.cpp
@@ -70,6 +70,7 @@ int HttpPortListener::bindServerConnection() {
 	this->address.sin_family = AF_INET;
 	this->address.sin_addr.s_addr = INADDR_ANY;
 	this->address.sin_port = htons(this->getPort());
+	// TODO: Exit when a port is invalid
 	if (bind(server_fd, (struct sockaddr *) &address, sizeof(this->address))) {// Socket to local address
 		std::cerr << __func__ << ":" << __LINE__ << ": error when binding the socket" << std::endl;
 		return (EXIT_FAILURE);
